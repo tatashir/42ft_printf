8ef008360b49e27208e7916e01257b85d78e8e2d
Taichi Tashiro <tatashir@c1r11s7.42tokyo.jp>
2022-12-05 09:16:12 +0900

    12050915

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,8 +6,26 @@
 /*   By: tatashir <tatashir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 16:38:19 by tatashir          #+#    #+#             */
-/*   Updated: 2022/11/28 16:40:36 by tatashir         ###   ########.fr       */
+/*   Updated: 2022/12/05 09:04:02 by tatashir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include 
+#include "ft_printf.h"
+
+int	ft_putstr(char *s)
+{
+	int	count;
+
+	count = 0;
+	if (!s)
+	{
+		count += ft_putstr("(null)");
+		return (count);
+	}
+	while (*s)
+	{
+		count += ft_putchar(*s);
+		s++;
+	}
+	return (count);
+}
